3d188690b4f6a2b04dd76da6ab2331d202644b26
annecie <ajbexcelsior@gmail.com>
2021-11-10 15:27:28 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //write your code here
+
+// Steps 
+//declare a function
+//set parameters
+// declare a new variable called newString initialize to an empty string.
+// create a for loop to checjk every character in a string.
+
+function stripUpperCase(string){
+  let stripped = '';
+  for (let i = 0; i< string.length; i++){
+      if (string[i] === string[i].toLowerCase())
+      stripped += string[i];
+  }
+    return stripped
+};
+
+console.log(stripUpperCase("Hello"));
